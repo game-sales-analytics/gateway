2d92773c48d208e696dffe481e6a71089ce1f443
Morteza Behboodian <29199390+xeptore@users.noreply.github.com>
2021-11-19 20:46:39 +0330

    feat: add sample ping request to main script

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import fastify from 'fastify';
 import { StatusCodes } from 'http-status-codes';
 import { pino } from 'pino';
+import { credentials } from '@grpc/grpc-js';
+import { PingRequest } from '../proto-gen/userssrv_pb';
+import { UsersServiceClient } from '../proto-gen/userssrv_grpc_pb';
 
 
 const server = fastify({
@@ -15,7 +18,7 @@ const server = fastify({
       },
     },
     timestamp: pino.stdTimeFunctions.isoTime,
-    level: 'debug',
+    level: 'trace',
   }),
 });
 
@@ -31,6 +34,18 @@ server.route({
 server.listen('3000', 'localhost')
   .then(address => {
     server.log.info('server is listening on', address);
+
+    server.log.trace("connection to users service grpc server at", "localhost:50050");
+
+    const client = new UsersServiceClient('localhost:50050', credentials.createInsecure());
+    client.ping(new PingRequest(), (error, response) => {
+      if (null !== error) {
+        server.log.error(error, "failed calling server");
+        throw error;
+      }
+
+      server.log.info({ pong: response.getPong() }, "received response");
+    })
   })
   .catch(error => {
     server.log.error('failed starting server', error);
