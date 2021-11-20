43b6413f8b903d1252d35da7566d7e14cef8cccd
Morteza Behboodian <29199390+xeptore@users.noreply.github.com>
2021-11-20 06:11:53 +0330

    feat: add grpc connection initializer

---
+++ src/grpc/users.ts
@@ -0,0 +1,29 @@
+import { credentials } from '@grpc/grpc-js';
+import { format } from 'node:util';
+import { UsersServiceClient } from '../../proto-gen/userssrv_grpc_pb';
+import { PingRequest } from '../../proto-gen/userssrv_pb';
+
+
+export interface UsersServiceConnectionConfig {
+  readonly host: string;
+  readonly port: number;
+}
+
+export async function connect(
+  config: UsersServiceConnectionConfig,
+): Promise<UsersServiceClient> {
+  const client = new UsersServiceClient(
+    format('%s:%s', config.host, config.port),
+    credentials.createInsecure(),
+  );
+
+  return await new Promise((resolve, reject) => {
+    client.ping(new PingRequest(), error => {
+      if (null !== error) {
+        return reject(error);
+      }
+
+      return resolve(client);
+    });
+  });
+}
