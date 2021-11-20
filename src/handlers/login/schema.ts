f396c51e2f15b781052c07b546e38db16a6c5de3
Morteza Behboodian <29199390+xeptore@users.noreply.github.com>
2021-11-20 07:26:22 +0330

    fix: add more rules to login form validation schema

--- src/handlers/login/schema.ts
+++ src/handlers/login/schema.ts
@@ -2,15 +2,16 @@ import Joi from 'joi';
 
 
 const schema = Joi.object({
-  password: Joi.string().min(1).required(),
+  password: Joi.string().trim(true).min(8, 'utf-8').required(),
   email: Joi.string()
+    .trim(true)
     .email({
       minDomainSegments: 2,
       tlds: { allow: ['com', 'net'] },
     })
     .required(),
-  deviceUserAgent: Joi.string().required(),
-  ip: Joi.string().ip({ version: 'ipv4' }).required()
+  deviceUserAgent: Joi.string().trim().required(),
+  ip: Joi.string().trim().ip({ version: 'ipv4' }).required(),
 });
 
 export default schema;
