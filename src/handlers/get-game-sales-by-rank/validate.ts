cddf2833343c742dea15ffcce389ba59aafe1ac1
Morteza Behboodian <29199390+xeptore@users.noreply.github.com>
2021-11-20 18:50:31 +0330

    fix: improve get-game-by-rank validation rules

--- src/handlers/get-game-sales-by-rank/validate.ts
+++ src/handlers/get-game-sales-by-rank/validate.ts
@@ -9,9 +9,23 @@ export default function validate(rank: unknown): void {
     (r: unknown) => Number.isFinite(Number.parseInt(r as string, 10)),
     (r: unknown) => Number.isSafeInteger(Number.parseInt(r as string, 10)),
     (r: unknown) => Number.isInteger(Number.parseInt(r as string, 10)),
+    (r: unknown) => /^\d{1,21}$/.test(r as string),
   ].every(pred => pred(rank))) {
     throw new ValidationError([
       { code: 'E_BAD_TYPE', path: 'rank' },
+    ]);
+  }
+
+  const rankInt = Number.parseInt(rank as string, 10);
+  if (rankInt <= 0) {
+    throw new ValidationError([
+      { code: 'E_MIN_LIMIT', path: 'rank' },
+    ]);
+  }
+
+  if (rankInt > Number.MAX_SAFE_INTEGER) {
+    throw new ValidationError([
+      { code: 'E_MAX_LIMIT', path: 'rank' },
     ]);
   }
 }
