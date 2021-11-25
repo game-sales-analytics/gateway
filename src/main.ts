96e997d81f091a5282152dbc1ea1ed9ad851a880
Morteza Behboodian <29199390+xeptore@users.noreply.github.com>
2021-11-25 19:54:09 +0330

    fix: read services connection paramters from env vars

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,17 @@ import { connect as connectUsersService } from './grpc/users';
 import { initialize } from './server';
 
 
-const USERS_SERVICE_PORT = 50_051;
-const CORE_SERVICE_PORT = 50_050;
-const SERVER_PORT = 8888;
+const USERS_SERVICE_HOST = process.env['GATEWAY_USERSSRV_HOST'] ?? 'localhost';
+const USERS_SERVICE_PORT = Number.parseInt(process.env['GATEWAY_USERSSRV_PORT'] ?? '50051', 10);
+const CORE_SERVICE_HOST = process.env['GATEWAY_CORESRV_HOST'] ?? 'localhost';
+const CORE_SERVICE_PORT = Number.parseInt(process.env['GATEWAY_CORESRV_PORT'] ?? '50050', 10);
+const SERVER_HOST = process.env['GATEWAY_LISTEN_HOST'] ?? 'localhost';
+const SERVER_PORT = Number.parseInt(process.env['GATEWAY_LISTEN_PORT'] ?? '6868', 10);
+
 
 void Promise.all([
-  connectUsersService({ host: 'localhost', port: USERS_SERVICE_PORT }),
-  connectCoreService({ host: 'localhost', port: CORE_SERVICE_PORT }),
+  connectUsersService({ host: USERS_SERVICE_HOST, port: USERS_SERVICE_PORT }),
+  connectCoreService({ host: CORE_SERVICE_HOST, port: CORE_SERVICE_PORT }),
 ])
   .then(async services => await initialize(...services))
-  .then(async server => await server.listen('localhost', SERVER_PORT));
+  .then(async server => await server.listen(SERVER_HOST, SERVER_PORT));
